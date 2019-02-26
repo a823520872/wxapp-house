99791f19fabb08dc49aa09d6fdd6414e0cc2d3c2
a823520872 <823520872@qq.com>
2019-02-26 22:49:07 +0800

    提交

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -7,12 +7,55 @@
                 </view>
         	</swiper-item>
         </swiper>
+		<view class="cells">
+			<view class="cells_hd">
+				
+			</view>
+			<view class="cells_bd">
+				
+			</view>
+			<view class="cells_fd">
+				<scroll-view class="scroll_view" :scroll-x="true">
+					<view class="m_flex">
+						<view class="category">
+							<view class="btn active">
+								单间
+							</view>
+						</view>
+						<view class="category">
+							<view class="btn">
+								一房一厅
+							</view>
+						</view>
+						<view class="category">
+							<view class="btn">
+								单间
+							</view>
+						</view>
+						<view class="category">
+							<view class="btn">
+								一房一厅
+							</view>
+						</view>
+						<view class="category">
+							<view class="btn">
+								单间
+							</view>
+						</view>
+						<view class="category">
+							<view class="btn">
+								一房一厅
+							</view>
+						</view>
+					</view>
+				</scroll-view>
+			</view>
+		</view>
 		<house-list :list.sync="list"></house-list>
 	</view>
 </template>
 
 <script>
-import { mapState, mapMutations, mapActions } from 'vuex';
 import HouseList from '../components/house-list';
 export default {
 	components: {
@@ -33,13 +76,11 @@ export default {
 		console.log('onReachBottom');
 	},
 	onReady() {
-		this.setInitFn(this.getData);
+		this.getData()
 	},
 	methods: {
-		...mapMutations(['setInitFn']),
 		getData() {
 			console.log('init');
-			this.setInitFn(null);
 		}
 	}
 };
@@ -56,4 +97,26 @@ export default {
             }
         }
     }
+	.scroll_view {
+		padding: 10upx 30upx;
+		.category {
+			position: relative;
+			width: 200upx;
+			height: 56upx;
+			margin: 5upx 30upx 5upx 0;
+		}
+		.btn {
+			width: 200upx;
+			height: 56upx;
+			line-height: 56upx;
+			background-color: #fff;
+			text-align: center;
+			font-size: 25upx;
+			color: $main-color;
+		}
+		.active {
+			background-color: $main-color;
+			color: #fff;
+		}
+	}
 </style>
